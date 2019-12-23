9f9f8167e26415ff5b1fdd088dfc6d94eb918a08
Adam Jin <scemsjyd@gmail.com>
2019-12-23 16:51:20 +0800

    commit

---
+++ spring-cloud-example-ribbon/src/main/java/com/scemsjyd/springcloudexample/ribbon/SpringCloudExampleRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.scemsjyd.springcloudexample.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class SpringCloudExampleRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudExampleRibbonApplication.class, args);
+    }
+}
